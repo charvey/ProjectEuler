11150aa97b98d076a897476295f48c7f8d231b7b
Chris Harvey <charvey@drexel.edu>
2012-05-24 16:37:00 -0400

    Problems 70 and 94 started

--- csharp/PE/Program.cs
+++ csharp/PE/Program.cs
@@ -7,7 +7,7 @@ namespace PE
     {
         static void Main(string[] args)
         {
-            SolveOne(084);
+            SolveOne(070);
         }
 
         public static void SolveOne(int n)
