3712aa3854664401f206423880f5cc516748a42d
Chris Harvey <charvey@drexel.edu>
2012-11-13 13:03:12 -0500

    Solved problem 068

--- js/Problem068.js
+++ js/Problem068.js
@@ -1,45 +1,56 @@
 function Problem068(){
-	var N = 3;
-	var D = 6;
-	
-	var poss = new Array(N*D+1);
-	for(var i=0;i<=N*D;i++) poss[i]=[];
-	
-	var s = [null,null,null];
-	for(var i=0;i<Math.pow(D,N);i++){
-		var n = i;
-		var sum = 0;
-		
-		for(var j=2;j>=0;j--){
-			s[j] = Math.floor(n/Math.pow(D,j));
-			n -= s[j]*Math.pow(D,j);
-			s[j]++;
-			sum+=s[j];
+	var R = 5;
+	var D = R*2;
+	var answer = 0;
+
+	var fact = function(n){return n==0?1:n*fact(n-1);};
+
+	var digits = [];
+	for(var d=1;d<=D;d++)digits.push(d);
+
+	var t = fact(D);
+	for(var i=0;i<t;i++){
+		var a = new Array(D);
+		var _digits = digits.slice(0);
+
+		var s = i;
+		for(var d=D-1;d>=0;d--){
+			var fd = fact(d);
+			var dt = Math.floor(s/fd);
+
+			a[d] = _digits.splice(dt,1)[0];
+
+			s -= dt*fd;
 		}
-		
-		poss[sum].push(s.slice());
-	}
-	
-	var sols = [];
-	
-	for(var i=1;i<=N*D;i++){
-		if(poss[i].length===0)
-			continue;
-	
-		var sol = new Array(N);
-		for(var j=0;j<N;j++) sol[j]=0;
-				
-		for(var j=0;j<N;j++){
-			for(var k=0;k<sols[i].length;k++){
-				if(sols[i][sols[j]][1]==sols[i][sols[j-1]][2]){
-					if(j==N-1 && sols[i][sols[j]][1]==sols[i][sols[j]][2])
-						continue;
-				}
+
+		var sum = null;
+		for(var r=0;r<R;r++){
+			var sub = a[2*r]+a[2*r+1]+a[(2*(r+1)+1)%D];
+
+			if(sum==null)
+				sum = sub;
+			else if(sum==sub)
+				continue;
+			else{
+				sum = null;
+				break;
 			}
 		}
-	}
-	
-	return poss;
-}
 
-Problem068()
+		if(sum!=null){
+			var minR=0;
+			for(var r=1;r<R;r++)
+				if(a[2*r]<a[2*minR])
+					minR = r;
+
+			var poss = "";
+			for(var r=0;r<R;r++)
+				poss += ""+a[2*(r+minR)]+a[2*(r+minR)+1]+a[(2*(r+minR+1)+1)%D];
+
+			if(poss.length==16 && parseInt(poss)>parseInt(answer))
+				answer = poss;
+		}
+	}
+
+	return answer;
+}
