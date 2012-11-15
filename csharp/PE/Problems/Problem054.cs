d5aa2e1b7a6eb926449dfac72264f1ea2d06d788
Chris Harvey <charvey@drexel.edu>
2012-11-15 16:40:50 -0500

    Solved 054

--- csharp/PE/Problems/Problem054.cs
+++ csharp/PE/Problems/Problem054.cs
@@ -17,17 +17,82 @@ namespace PE.Problems
             var cards = GetFileLines("http://projecteuler.net/project/poker.txt").Select(l => l.Split(' ').Select(ParseCard));
             var hands = cards.Select(cs => new Tuple<Card[], Card[]>(cs.Take(5).ToArray(), cs.Skip(5).Take(5).ToArray()));
             var scores = hands.Select(h => new Tuple<HandScore, HandScore>(ScoreHand(h.Item1), ScoreHand(h.Item2)));
-            int wins = scores.Count(hs => hs.Item1.Class > hs.Item2.Class || (hs.Item1.Class == hs.Item2.Class && hs.Item1.HighRank > hs.Item2.HighRank));
+
+            int wins = scores.Count(hs => hs.Item1.Class > hs.Item2.Class ||
+                                          (hs.Item1.Class == hs.Item2.Class && hs.Item1.ClassRank > hs.Item2.ClassRank) ||
+                                          (hs.Item1.Class == hs.Item2.Class && hs.Item1.ClassRank == hs.Item2.ClassRank &&
+                                           hs.Item1.HighRank > hs.Item2.HighRank));
 
             return wins;
         }
 
-        private readonly List<char> Ranks = new List<char> {'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'};
+        private readonly List<char> Ranks = new List<char> { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' };
         private readonly List<char> Suits = new List<char> {'C', 'S', 'H', 'D'};
+
+        private string handstring(Card[] hand)
+        {
+            return
+                hand.Select(c => Ranks[c.Rank] + "" + Suits[(int) c.Suit])
+                    .Aggregate("", (h, c) => h + " " + c)
+                    .Remove(0, 1);
+        }
 
         HandScore ScoreHand(Card[] hand)
         {
-            return new HandScore();
+            HandClass handClass = HandClass.HighCard;
+            int classRank = 0;
+
+            bool flush = hand.GroupBy(c => c.Suit).Max(g => g.Count()) == 5;
+            bool straight = hand.Select(c => c.Rank).Distinct().Count() == 5 &&
+                            hand.OrderBy(c => c.Rank).Max(c => c.Rank) - hand.OrderBy(c => c.Rank).Min(c => c.Rank) == 4;
+
+            if (flush && straight && hand.OrderBy(c => c.Rank).Max().Rank == Ranks.Count-1)
+            {
+                handClass=HandClass.RoyalFlush;
+                classRank = Ranks.Count - 1;
+            }
+            else if(flush&&straight)
+            {
+             handClass=HandClass.StraightFlush;
+                classRank = hand.Max(c => c.Rank);
+            }
+            else if (hand.GroupBy(c => c.Rank).Max(g => g.Count()) == 4)
+            {
+                handClass = HandClass.FourKind;
+                classRank = hand.GroupBy(c => c.Rank).OrderByDescending(g => g.Count()).First().First().Rank;
+            }
+            else if (hand.GroupBy(c => c.Rank).Select(g => g.Count()).All(c => c == 2 || c == 3))
+            {
+                handClass = HandClass.FullHouse;
+                classRank = hand.GroupBy(c => c.Rank).OrderByDescending(g => g.Count()).First().First().Rank;
+            }
+            else if (flush)
+            {
+                handClass = HandClass.Flush;
+                classRank = hand.Max(c => c.Rank);
+            }
+            else if (straight)
+            {
+                handClass = HandClass.Straight;
+                classRank = hand.Max(c => c.Rank);
+            }
+            else if (hand.GroupBy(c => c.Rank).Max(g => g.Count()) == 3)
+            {
+                handClass = HandClass.ThreeKind;
+                classRank = hand.GroupBy(c => c.Rank).OrderByDescending(g => g.Count()).First().First().Rank;
+            }
+            else if (hand.GroupBy(c => c.Rank).Count(g => g.Count() == 2) == 2)
+            {
+                handClass = HandClass.TwoPair;
+                classRank = hand.GroupBy(c => c.Rank).Where(g => g.Count() == 2).OrderByDescending(g => g.First().Rank).First().First().Rank;
+            }
+            else if (hand.GroupBy(c => c.Rank).Any(g => g.Count() == 2))
+            {
+                handClass = HandClass.OnePair;
+                classRank = hand.GroupBy(c => c.Rank).Where(g => g.Count() == 2).OrderByDescending(g => g.First().Rank).First().First().Rank;
+            }
+
+            return new HandScore {Class = handClass, ClassRank = classRank, HighRank = hand.Max(c => c.Rank)};
         }
 
         Card ParseCard(string c)
@@ -40,8 +105,23 @@ namespace PE.Problems
 
         private struct HandScore
         {
-            public int Class;
+            public HandClass Class;
+            public int ClassRank;
             public int HighRank;
+        }
+
+        private enum HandClass
+        {
+            HighCard = 0,
+            OnePair = 1,
+            TwoPair = 2,
+            ThreeKind = 3,
+            Straight = 4,
+            Flush = 5,
+            FullHouse = 6,
+            FourKind = 7,
+            StraightFlush = 8,
+            RoyalFlush = 9
         }
 
         private enum Suit
